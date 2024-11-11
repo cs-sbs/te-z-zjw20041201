e5862dea7f7e3e96b940948795a3a5cd590af607
zjw120 <zjw120@github.com>
2024-11-11 20:00:54 +0800

    update assignment

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -13,5 +13,20 @@ public class NumberFeature {
         result = inputNumber;
 
         System.out.println(result);
+        public static void digital_eigenvalue(int inputNumber, int result) {
+            int number;
+            int digit_position = 1;
+            int binary_bit_value = 1;
+            while (inputNumber > 0) {
+                number = inputNumber % 10;
+                inputNumber = inputNumber / 10;
+                if (number % 2 == digit_position % 2)
+                    result += binary_bit_value;
+                binary_bit_value *= 2;
+                digit_position++;
+            }
+            System.out.println(result);
+        }
+
     }
 }
